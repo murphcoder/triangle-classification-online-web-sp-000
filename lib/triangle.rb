5abf0ade0820a0b1bbb783dc208da9218aa4fc0b
Sean Murphy <filmmakersean@fastmail.net>
2019-08-11 01:10:07 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   end
   
   def kind
-    if @a <= 0 || @b <= 0 || @c <= 0 || @a >= @b + @c || @b >= @a + @c || @b >= @a + @c
+    if @a <= 0 || @b <= 0 || @c <= 0 || @a >= @b + @c || @b >= @a + @c || @c >= @a + @b
       raise TriangleError
       elsif @a == @b && @a == @c
       :equilateral
