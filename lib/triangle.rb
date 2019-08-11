4282a833a1c3366f79e42691222d86a8bf1019ec
Sean Murphy <filmmakersean@fastmail.net>
2019-08-11 01:11:21 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  
+  class TriangleError < StandardError
+    
+  end
+  
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    if @a <= 0 || @b <= 0 || @c <= 0 || @a >= @b + @c || @b >= @a + @c || @c >= @a + @b
+      raise TriangleError
+      elsif @a == @b && @a == @c
+      :equilateral
+      elsif @a == @b || @b == @c || @a == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
 end
