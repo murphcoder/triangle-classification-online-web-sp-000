1a6a47b413ebdfd0b507271b90824fb4e1afc0f0
Sean Murphy <filmmakersean@fastmail.net>
2019-08-11 00:59:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,24 @@
 class Triangle
   
+  class TriangleError < StandardError
+    
+  end
   
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    if @a == @b && @a == @c
+      :equilateral
+      elsif @a == @b || @b == @c || @a == @c
+      :isosceles
+      elsif @a + @b > @c && @a + @c > @b && @b + @c > @a && @a > 0 && @b > 0 && @c > 0
+      :scalene
+    else
+      
+  end
   
 end
