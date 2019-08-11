c88a4f45283f99b8603d5da078910d85ba58ea91
Sean Murphy <filmmakersean@fastmail.net>
2019-08-11 01:07:31 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,14 +11,13 @@ class Triangle
   end
   
   def kind
-    if @a == @b && @a == @c
+    if @a <= 0 || @b <= 0 || @c <= 0 || @a >= @b + @c || @b >= @a + @c || @b >= @a + @c
+      
+      elsif @a == @b && @a == @c
       :equilateral
       elsif @a == @b || @b == @c || @a == @c
       :isosceles
-      elsif @a + @b > @c && @a + @c > @b && @b + @c > @a && @a > 0 && @b > 0 && @c > 0
-      :scalene
-    else
-      raise TriangleError
+      
     end
   end
   
